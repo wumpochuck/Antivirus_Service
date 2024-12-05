103cfec1362579debcd280945912a73323e7c026
Maxim <130181963+wumpochuck@users.noreply.github.com>
2024-12-05 22:07:33 +0300

    testing

--- Antivirus_Service/Antivirus_Service.cpp
+++ Antivirus_Service/Antivirus_Service.cpp
@@ -1,7 +1,62 @@
+п»ї#include <windows.h>
+#include <string>
+#include <curl/curl.h>
 #include "Antivirus_Service.h"
 #include "Logger.h"
 
+#pragma comment(lib, "libcurl.lib")
+
 #define PIPE_NAME _T("\\\\.\\pipe\\AntivirusServicePipe")
+#define SERVER_IP "192.168.0.21"
+#define SERVER_PORT 8080
+
+std::string ConvertToUTF8(const std::wstring& wstr)
+{
+    if (wstr.empty()) return std::string();
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+    std::string strTo(size_needed, 0);
+    WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+    return strTo;
+}
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+std::string SendPostRequest(const std::string& url, const std::string& data)
+{
+    CURL* curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl)
+    {
+        struct curl_slist* headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+        {
+            Log(LogLevel::ERR, "AntivirusService: SendPostRequest: curl_easy_perform() failed: " + std::string(curl_easy_strerror(res)));
+        }
+
+        curl_easy_cleanup(curl);
+        curl_slist_free_all(headers);
+    }
+    curl_global_cleanup();
+
+    return readBuffer;
+}
 
 int _tmain(int argc, TCHAR* argv[])
 {
@@ -168,16 +223,14 @@ DWORD WINAPI ServiceWorkerThread(LPVOID lpParam)
     while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0)
     {
         HANDLE hPipe = CreateNamedPipe(
-            PIPE_NAME,             // имя канала
-            PIPE_ACCESS_DUPLEX,    // двусторонний канал
-            PIPE_TYPE_MESSAGE |    // тип сообщения
-            PIPE_READMODE_MESSAGE |// режим чтения сообщения
-            PIPE_WAIT,             // синхронный режим
-            PIPE_UNLIMITED_INSTANCES, // максимальное количество экземпляров
-            512,                   // размер выходного буфера
-            512,                   // размер входного буфера
-            0,                     // тайм-аут клиента
-            NULL);                 // атрибуты безопасности
+            PIPE_NAME,
+            PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+            PIPE_UNLIMITED_INSTANCES,
+            512,
+            512,
+            0,
+            NULL);
 
         if (hPipe == INVALID_HANDLE_VALUE)
         {
@@ -200,32 +253,63 @@ DWORD WINAPI ServiceWorkerThread(LPVOID lpParam)
         char buffer[512];
         DWORD bytesRead;
 
-        while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0)
+        BOOL result = ReadFile(hPipe, buffer, sizeof(buffer) - 1, &bytesRead, NULL);
+        if (result && bytesRead != 0)
         {
-            BOOL result = ReadFile(
-                hPipe,        // дескриптор канала
-                buffer,       // буфер для чтения
-                sizeof(buffer) - 1, // размер буфера
-                &bytesRead,   // количество прочитанных байт
-                NULL);        // не используется
+            buffer[bytesRead] = '\0';
+            Log(LogLevel::INFO, buffer);
 
-            if (result && bytesRead != 0)
+            std::string data(buffer);
+            size_t pos = data.find("register:");
+            if (pos != std::string::npos)
             {
-                buffer[bytesRead] = '\0'; // добавляем нуль-терминатор
-                Log(LogLevel::INFO, buffer); // записываем данные в лог
-            }
-            else
-            {
-                DWORD error = GetLastError();
-                if (error == ERROR_BROKEN_PIPE)
+                Log(LogLevel::INFO, "AntivirusService: ServiceWorkerThread: Register request");
+                std::string credentials = data.substr(pos + 9);
+                size_t loginEnd = credentials.find(':');
+                size_t passwordEnd = credentials.find(':', loginEnd + 1);
+                size_t emailEnd = credentials.find(':', passwordEnd + 1);
+
+                Log(LogLevel::INFO, "AntivirusService: ServiceWorkerThread: Parsed credentials - loginEnd: " + std::to_string(loginEnd) + ", passwordEnd: " + std::to_string(passwordEnd) + ", emailEnd: " + std::to_string(emailEnd));
+
+                if (loginEnd != std::string::npos && passwordEnd != std::string::npos && emailEnd != std::string::npos)
                 {
-                    Log(LogLevel::INFO, "AntivirusService: ServiceWorkerThread: Client disconnected");
+                    std::string login = credentials.substr(0, loginEnd);
+                    std::string password = credentials.substr(loginEnd + 1, passwordEnd - loginEnd - 1);
+                    std::string email = credentials.substr(passwordEnd + 1);
+
+                    Log(LogLevel::INFO, "AntivirusService: ServiceWorkerThread: Parsed values - login: " + login + ", password: " + password + ", email: " + email);
+
+                    std::string json = "{\"login\":\"" + login + "\",\"password\":\"" + password + "\",\"email\":\"" + email + "\"}";
+
+                    Log(LogLevel::INFO, "AntivirusService: ServiceWorkerThread: JSON data: " + json);
+
+                    Log(LogLevel::INFO, "AntivirusService: ServiceWorkerThread: Sending request to server");
+
+                    std::string response = SendPostRequest("http://" SERVER_IP ":8080/auth/register", json);
+
+                    Log(LogLevel::INFO, "AntivirusService: ServiceWorkerThread: Response from server: " + response);
+
+                    DWORD bytesWritten;
+                    WriteFile(hPipe, response.c_str(), response.length(), &bytesWritten, NULL);
+
+                    Log(LogLevel::INFO, "AntivirusService: ServiceWorkerThread: Response sent to client");
                 }
                 else
                 {
-                    Log(LogLevel::ERR, "AntivirusService: ServiceWorkerThread: ReadFile failed with error " + std::to_string(error));
+                    Log(LogLevel::ERR, "AntivirusService: ServiceWorkerThread: Invalid credentials format");
                 }
-                break; // ошибка чтения или разрыв соединения
+            }
+        }
+        else
+        {
+            DWORD error = GetLastError();
+            if (error == ERROR_BROKEN_PIPE)
+            {
+                Log(LogLevel::INFO, "AntivirusService: ServiceWorkerThread: Client disconnected");
+            }
+            else
+            {
+                Log(LogLevel::ERR, "AntivirusService: ServiceWorkerThread: ReadFile failed with error " + std::to_string(error));
             }
         }
 
@@ -237,3 +321,4 @@ DWORD WINAPI ServiceWorkerThread(LPVOID lpParam)
 
     return ERROR_SUCCESS;
 }
+
